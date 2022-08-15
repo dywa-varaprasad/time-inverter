8becdb030856f454c5a095bb66cfb2f4e9e68a24
DywaVaraprasad <105714346+dywa-varaprasad@users.noreply.github.com>
2022-08-15 23:18:04 +0530

    have coded hard but will update it soon

---
+++ timeConverter.js
@@ -0,0 +1,17 @@
+function IndToUs(indTime){
+    //return indTime-9.5;
+  
+if(indTime>12){
+  let time=(24-indTime)-9.5;
+  return time
+//document.createElement(h2)
+ }
+  else if (indTime<12){
+ return indTime-9.5
+ }
+
+//else if(indTime>24){
+  //indTime=24-12-9.5
+
+ console.log(IndToUs(2))
+ console.log(IndToUs(2))
